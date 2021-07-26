ffdee7c8fdc5c1c295d17b80c96b0c2ad57673db
andrei.aliokhin <andrewaliokhin@gmail.com>
2021-07-26 17:04:25 +0300

    26.07commit

---
+++ src/HelloWorld/Hello.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int myFirsNumber;
+        myFirsNumber = 5+10;
+        System.out.println(myFirsNumber);
+
+    }
+}
